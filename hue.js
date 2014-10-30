5d71616c330118e75fb11e25727c2b0c7d766bf1
cabox <cabox@box-codeanywhere.com>
2014-10-30 17:50:48 -0400

    Hue

--- hue.js
+++ hue.js
@@ -1,16 +1,22 @@
-var hue = require("node-hue-api");
+var HueApi = require("node-hue-api").HueApi;
 
-var displayBridges = function(bridge) {
-    console.log("Hue Bridges Found: " + JSON.stringify(bridge));
+var hostname = "192.168.2.129",
+    newUserName = null; // You can provide your own username value, but it is normally easier to leave it to the Bridge to create it
+    userDescription = "device description goes here";
+
+var displayUserResult = function(result) {
+    console.log("Created user: " + JSON.stringify(result));
 };
 
-// --------------------------
-// Using a promise
-hue.locateBridges().then(displayBridges).done();
+var displayError = function(err) {
+    console.log(err);
+};
+
+var hue = new HueApi();
 
 // --------------------------
-// Using a callback
-hue.locateBridges(function(err, result) {
+// Using a callback (with default description and auto generated username)
+hue.createUser(hostname, null, null, function(err, user) {
     if (err) throw err;
-    displayBridges(result);
+    displayUserResult(user);
 });
